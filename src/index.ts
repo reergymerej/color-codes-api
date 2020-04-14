214674f7bc22b594e276efada9f569aa9ccafcf0
Jeremy Greer <jex.grizzle@gmail.com>
2020-04-14 13:06:33 -0700

    Add route to add color

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import Koa from 'koa'
 import koaRoute from 'koa-route'
 
-
-
-
 const colors = [
   {id: 1},
   {id: 2},
 ]
 
-
 const app = new Koa()
 
-
-const listColors = async ctx => {
+const listColors = async (ctx: Koa.Context) => {
   ctx.body = colors
 }
 
+const addColor = async (ctx: Koa.Context) => {
+  const newColor = {id: colors.length}
+  colors.push(newColor)
+  ctx.body = newColor
+}
+
 app.use(koaRoute.get('/colors', listColors))
+app.use(koaRoute.post('/colors', addColor))
 
 app.listen(3000)
