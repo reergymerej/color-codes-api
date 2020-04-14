1f645375cd7554a505bd446c8cb42c023667aba7
Jeremy Greer <jex.grizzle@gmail.com>
2020-04-14 12:50:29 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint/eslint-plugin"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,25 @@
+# colorcode-api
+
+* tests - `yarn test`
+* watch - `yarn watch`
+* dev mode - `yarn dev --inspect`
+
+
+## db - mongo
+
+We don't have any relationships to worry about right now.
+
+
+## orm - mongoose
+
+Obvious option
+
+
+## Node
+
+What we want to play with...
+
+
+## Koa
+
+Something different, minimal
