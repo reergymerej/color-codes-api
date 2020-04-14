c08aee830cb72dfe85f0e126f824a9b90c578cea
Jeremy Greer <jex.grizzle@gmail.com>
2020-04-14 13:20:52 -0700

    Pull value from POST body

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,42 @@
 import Koa from 'koa'
 import koaRoute from 'koa-route'
+import bodyParser from 'koa-bodyparser'
 
-const colors = [
-  {id: 1},
-  {id: 2},
+
+
+type Color = {
+  id: number;
+  value: string;
+}
+
+const colors: Color[] = [
+  {id: 1, value: 'red'},
+  {id: 2, value: 'green'},
 ]
 
 const app = new Koa()
+app.use(bodyParser())
 
 const listColors = async (ctx: Koa.Context) => {
   ctx.body = colors
 }
 
+type AddColorReqDTO = {
+  value: string;
+}
+
 const addColor = async (ctx: Koa.Context) => {
-  const newColor = {id: colors.length}
-  colors.push(newColor)
-  ctx.body = newColor
+  const body: AddColorReqDTO = ctx.request.body
+
+  // TODO: throw for bad request
+  if (body.value) {
+    const newColor: Color = {
+      id: colors.length,
+      value: body.value,
+    }
+    colors.push(newColor)
+    ctx.body = newColor
+  }
 }
 
 app.use(koaRoute.get('/colors', listColors))
